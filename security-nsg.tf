eb1a4425893ba25db950a051c2691ac7d53b5ff3
Ratan Mohapatra <ratanm@live.ca>
2026-04-12 10:43:45 -0400

    set guac access to my ip

--- security-nsg.tf
+++ security-nsg.tf
@@ -106,7 +106,7 @@ resource "azurerm_network_security_rule" "public_https" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "443"
-  source_address_prefix       = "*"
+  source_address_prefix       = var.my_ip
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.this.name
   network_security_group_name = azurerm_network_security_group.nsg_public.name
